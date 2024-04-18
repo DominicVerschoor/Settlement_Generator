ba065bb48d76d934d81b6692c5986929550c5037
DominicVerschoor <91744699+DominicVerschoor@users.noreply.github.com>
2024-04-18 09:31:43 +0200

    updated fitness to work with arrays to avoid timeout errors for too many requests

--- fitness.py
+++ fitness.py
@@ -1,84 +1,45 @@
 import sys
 import numpy as np
 import os
-from gdpc import __url__, Editor, Block
+import time
+from gdpc import __url__, Block
 from gdpc.exceptions import InterfaceConnectionError, BuildAreaNotSetError
 from gdpc.vector_tools import addY
 from glm import ivec2, ivec3
 from generateRandomSample import generateRandomSample
 
+import random
 
-class fitness:
 
-    def __init__(self, building_locations):
+class Fitness:
+
+    def __init__(self, building_locations, map):
         """
         Initializes the class instance.
         """
-        self.editor = Editor()
-        self.check_editor_connection()
-        self.initialize_slice()
         self.building_locations = building_locations
-
-    def check_editor_connection(self):
-        """
-        Checks the connection to the GDMC HTTP interface.
-        """
-        try:
-            self.editor.checkConnection()
-        except InterfaceConnectionError:
-            print(
-                f"Error: Could not connect to the GDMC HTTP interface at {self.editor.host}!\n"
-                'To use GDPC, you need to use a "backend" that provides the GDMC HTTP interface.\n'
-                "For example, by running Minecraft with the GDMC HTTP mod installed.\n"
-                f"See {__url__}/README.md for more information."
-            )
-            sys.exit(1)
-
-    def initialize_slice(self):
-        """
-        Initializes the world slice.
-        """
-        try:
-            self.buildArea = self.editor.getBuildArea()
-        except BuildAreaNotSetError:
-            print(
-                "Error: failed to get the build area!\n"
-                "Make sure to set the build area with the /setbuildarea command in-game.\n"
-                "For example: /setbuildarea ~0 0 ~0 ~64 200 ~64"
-            )
-            sys.exit(1)
-
-        print("Loading world slice...")
-        self.buildRect = self.buildArea.toRect()
-        self.worldSlice = self.editor.loadWorldSlice(self.buildRect)
-        print("World slice loaded!")
+        self.map = map
 
     def check_floating(self, current_building):
-        float_counter = 0
-        non_solid_blocks = [
-            "minecraft:air",
-            "minecraft:amethyst_bud",
-        ]
         _, x_pos, y_pos, z_pos, max_x, _, max_z = current_building
+        x_values, y_values, z_values = zip(*[(x, y, z) for _, x, y, z in self.map])
+        map_coords = list(zip(x_values, y_values, z_values))
 
-        for x in range(x_pos, max_x + 1):
-            for z in range(z_pos, max_z + 1):
-                below_block = self.editor.getBlock((x, y_pos - 1, z))
+        for x in range(x_pos, x_pos + max_x + 1):
+            for z in range(z_pos, z_pos + max_z + 1):
+                if (x, y_pos - 1, z) not in map_coords:
+                    return 1
 
-                if str(below_block) in non_solid_blocks:
-                    float_counter += 1
+        return 0
 
-        return float_counter
-
-    def check_overlap(self, current_building):
+    def check_overlap(self, current_index, current_building):
+        # returns +1 for each overlapping building
         # returns number of overlapping blocks (*2 for each block because both are touching each other)
         overlap_counter = 0
         _, x_pos, y_pos, z_pos, max_x, max_y, max_z = current_building
 
-        for building in self.building_locations:
-            if (
-                building != current_building
-            ):  # Skip checking against the current building
+        for index, building in enumerate(self.building_locations):
+            if index != current_index:  # Skip checking against the current building
                 building_x, building_y, building_z = (
                     building[1],
                     building[2],
@@ -90,16 +51,31 @@ class fitness:
                     building_z + building[6],
                 )
 
-                for x in range(building_x, building_max_x + 1):
-                    for y in range(building_y, building_max_y + 1):
-                        for z in range(building_z, building_max_z + 1):
-                            if (
-                                x_pos <= x <= x_pos + max_x
-                                and y_pos <= y <= y_pos + max_y
-                                and z_pos <= z <= z_pos + max_z
-                            ):
-                                overlap_counter += 1
+                #### ALTERNATIVE ONLY CHECK OVERLAP REGION###
+                # Determine the overlapping region
+                # overlap_x_min = max(x_pos, building_x)
+                # overlap_x_max = min(x_pos + max_x, building_max_x)
+                # overlap_y_min = max(y_pos, building_y)
+                # overlap_y_max = min(y_pos + max_y, building_max_y)
+                # overlap_z_min = max(z_pos, building_z)
+                # overlap_z_max = min(z_pos + max_z, building_max_z)
 
+                # # Check for overlap in the overlapping region
+                # for x in range(overlap_x_min, overlap_x_max + 1):
+                #     for y in range(overlap_y_min, overlap_y_max + 1):
+                #         for z in range(overlap_z_min, overlap_z_max + 1):
+                #             overlap_counter += 1
+
+                # Check for overlap
+                if (
+                    x_pos <= building_max_x
+                    and x_pos + max_x >= building_x
+                    and y_pos <= building_max_y
+                    and y_pos + max_y >= building_y
+                    and z_pos <= building_max_z
+                    and z_pos + max_z >= building_z
+                ):
+                    overlap_counter += 1
         return overlap_counter
 
     def check_diversity(self, building_name):
@@ -107,13 +83,23 @@ class fitness:
         return sum(1 for item in self.building_locations if item[0] == building_name)
 
     def check_liquid_obstruction(self, current_building):
-        liquid_counter = 0
         liquid_blocks = [
             "minecraft:water",
             "minecraft:lava",
             "minecraft:bubble",
         ]
         _, x_pos, y_pos, z_pos, max_x, max_y, max_z = current_building
+        x_values = []
+        y_values = []
+        z_values = []
+
+        for name, x, y, z in self.map:
+            if any(liquid_block in str(name) for liquid_block in liquid_blocks):
+                x_values.append(x)
+                y_values.append(y)
+                z_values.append(z)
+
+        map_coords = list(zip(x_values, y_values, z_values))
 
         for x in range(x_pos, x_pos + max_x + 1):
             for y in range(y_pos, y_pos + max_y + 1):
@@ -128,39 +114,41 @@ class fitness:
                         or z == max_z
                     ):
 
-                        # Check the location above
+                        # # Check the surrounding blocks
                         adjacent_blocks = [
-                            self.editor.getBlock((x, y + 1, z)),
-                            self.editor.getBlock((x, y - 1, z)),
-                            self.editor.getBlock((x, y, z - 1)),
-                            self.editor.getBlock((x, y, z + 1)),
-                            self.editor.getBlock((x - 1, y, z)),
-                            self.editor.getBlock((x + 1, y, z)),
+                            (x, y, z),
+                            (x, y + 1, z),
+                            (x, y - 1, z),
+                            (x, y, z - 1),
+                            (x, y, z + 1),
+                            (x - 1, y, z),
+                            (x + 1, y, z),
                         ]
 
-                        if any(
-                            str(block) in liquid_blocks for block in adjacent_blocks
-                        ):
-                            liquid_counter += 1
+                        for current_block in adjacent_blocks:
+                            if current_block in map_coords:
+                                return 1
 
-        return liquid_counter
+        return 0
 
     def total_fitness(self):
         fitness, diversity, floating, liquid, overlap = (0, 0, 0, 0, 0)
-        for building in self.building_locations:
+        for index, building in enumerate(self.building_locations):
             diversity += self.check_diversity(building[0])
             floating += self.check_floating(building)
             liquid += self.check_liquid_obstruction(building)
-            overlap += self.check_overlap(building)
-        fitness = diversity + floating + liquid + overlap
-        return fitness, diversity, floating, liquid, overlap
+            overlap += self.check_overlap(index, building)
+        fitness = -diversity + floating + liquid + overlap
+        return fitness
 
 
 if __name__ == "__main__":
     sample = generateRandomSample()
-    sample.generate_buildings(dataset="BuildingDataSet", num_building=5)
+    # sample.generate_buildings(dataset="BuildingDataSet", num_buildings=1)
+    map = sample.map_to_array()
+    sample.choose_generated_buildings(
+        [2, 56, 56, -205, -205, "spruce.csv", "spruce.csv"], 2
+    )
 
-    b_loc = sample.get_building_locations()
-
-    fit = fitness(b_loc)
+    fit = Fitness(sample.building_locations, map)
     print(fit.total_fitness())
